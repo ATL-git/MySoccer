c359b593083500be384b64032da423f3e320d9fd
xoxoATLxoxo <a.maureau13400@gmailcom>
2024-07-31 15:52:32 +0200

    ajout fenetre invitplayer et nodemailer

--- publics/assets/script/teamsManager.js
+++ publics/assets/script/teamsManager.js
@@ -14,15 +14,15 @@ function closeModal() {
 
 
 function teamAddModal() {
-    modalBody.innerHTML="";
-    let divTeamAdd = document.createElement("div");
-    modalBody.appendChild(divTeamAdd);
-    divTeamAdd.classList.add('teamCreate');
+    modalBody.innerHTML = "";
+    let containerModalDiv = document.createElement("div");
+    modalBody.appendChild(containerModalDiv);
+    containerModalDiv.classList.add('containerModalDiv');
     let title = document.createElement('h2');
-    title.textContent="Création d'équipe";
-    divTeamAdd.appendChild(title);
+    title.textContent = "Création d'équipe";
+    containerModalDiv.appendChild(title);
     let form = document.createElement("form");
-    divTeamAdd.appendChild(form)
+    containerModalDiv.appendChild(form)
     form.setAttribute('action', '/teamAdd');
     form.setAttribute('method', 'post');
     let div = document.createElement('div');
@@ -45,6 +45,54 @@ function teamAddModal() {
 }
 
 
-function addPlayer(){
-    
+function addPlayer() {
+    modalBody.innerHTML = "";
+    let divPlayerAdd = document.createElement("div");
+    modalBody.appendChild(divPlayerAdd);
+    divPlayerAdd.classList.add('containerModalDiv');
+    let title = document.createElement('h2');
+    title.textContent = "Ajout de joueur";
+    divPlayerAdd.appendChild(title);
+    let divBtn = document.createElement('div');
+    divBtn.classList.add('divBtn')
+    divPlayerAdd.appendChild(divBtn);
+    let invitPlayerBtn = document.createElement('button');
+    divBtn.appendChild(invitPlayerBtn);
+    invitPlayerBtn.setAttribute('onclick', 'invitPlayer()');
+    invitPlayerBtn.textContent = "Inviter un joueur";
+    let createPlayerBtn = document.createElement('button');
+    divBtn.appendChild(createPlayerBtn);
+    createPlayerBtn.setAttribute('onclick', 'createPlayer()');
+    createPlayerBtn.textContent = "Crée un joueur";
+
+    openModal()
+}
+
+function invitPlayer() {
+    modalBody.innerHTML = "";
+    let containerModalDiv = document.createElement("div");
+    modalBody.appendChild(containerModalDiv);
+    containerModalDiv.classList.add('containerModalDiv');
+    let title = document.createElement('h2');
+    title.textContent = "Inviter un joueur";
+    containerModalDiv.appendChild(title);
+    let form = document.createElement("form");
+    containerModalDiv.appendChild(form)
+    form.setAttribute('action', '/invitPlayer');
+    form.setAttribute('method', 'post');
+    let div = document.createElement('div');
+    form.appendChild(div);
+    let label = document.createElement('label');
+    let input = document.createElement('input');
+    div.appendChild(label);
+    div.appendChild(input);
+    label.setAttribute('for', 'mail');
+    label.textContent = "Adresse mail du joueur :";
+    input.setAttribute('type', 'email');
+    input.setAttribute('id', 'mail');
+    input.setAttribute('name', 'mail');
+    let button = document.createElement('button');
+    form.appendChild(button);
+    button.setAttribute('type', 'submit');
+    button.textContent = 'Valider';
 }
--- routers/teamsRouter.js
+++ routers/teamsRouter.js
@@ -2,11 +2,12 @@ const authGuard = require('../middleware/authguard');
 const teamsRouter = require('express').Router();
 const teamsModel = require('../models/teamsModel');
 const usersModel = require("../models/usersModel");
+const nodemailer = require('nodemailer');
 
 teamsRouter.get('/teamsManager', authGuard, async (req, res) => {
     const teamsFinded = await usersModel.findById(req.session.user._id).populate({
         path: "teams",
-    }) 
+    })
     res.render('pages/teamsManager.twig', {
         user: req.session.user,
         teams: teamsFinded.teams
@@ -41,9 +42,43 @@ teamsRouter.get("/teamdelete/:teamid", authGuard, async (req, res) => {
             user: await userModel
                 .findById(req.session.user)
                 .populate("teams"),
-           
+
         });
     }
 });
+
+teamsRouter.post("/invitPlayer", authGuard, (req, res) => {
+    const nameAdmin = req.session.user.firstname
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+
+    const mailOptions = {
+
+        from: 'no-reply@mySoccer.com',
+        to: req.body.mail,
+        subject: 'Invitation pour MySoccer',
+        html: `<p>Veuillez cliquer sur le lien suivant pour accepter l'invitation de ${nameAdmin} : <a href='http://localhost:3000/acceptMailInvite'>Cliquez ici</a></p>`
+    };
+
+    transporter.sendMail(mailOptions, (error) => {
+        if (error) {
+            res.render("pages/teamsManager.twig", {
+                user: req.session.user,
+                errorMessage: "Une erreur est survenue lors de l'envoi de l'email",
+            });
+        } else {
+            res.render("pages/teamsManager.twig", {
+                user: req.session.user,
+                PMailSend: "Email envoyé avec succès !",
+            });
+        }
+    });
+});
+
 
 module.exports = teamsRouter
